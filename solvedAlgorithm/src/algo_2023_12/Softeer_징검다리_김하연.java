1915990919697d0b0a463df435a2e6a7462bfa53
hayeooo <hayeon0709@daum.net>
2023-12-13 09:17:21 +0900

    solved: 징검다리 LIS

--- solvedAlgorithm/src/algo_2023_12/Softeer_징검다리_김하연.java
+++ solvedAlgorithm/src/algo_2023_12/Softeer_징검다리_김하연.java
@@ -1,5 +1,11 @@
 package algo_2023_12;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
 /**
  * Softeer 징검다리
  * [ 문제 ]
@@ -14,7 +20,55 @@ package algo_2023_12;
  */
 public class Softeer_징검다리_김하연 {
 
-    public static void main(String[] args){
+    static BufferedReader br;
+    static StringTokenizer st;
 
+    static int N;
+    static int[] arr;
+    static int[] dp;
+
+    public static void main(String[] args) throws IOException{
+        br=new BufferedReader(new InputStreamReader(System.in));
+
+        // N 값을 입력받는다.
+        N=Integer.parseInt(br.readLine().trim());
+
+        arr=new int[N];
+        dp=new int[N];
+
+        // 돌의 높이를 입력받는다.
+        st=new StringTokenizer(br.readLine().trim());
+        for (int idx=0;idx<N;idx++){
+            arr[idx]=Integer.parseInt(st.nextToken());
+        }
+        int LIS=0;
+        for (int idx=0;idx<N;idx++){
+            int res=binarySearch(arr[idx],0,LIS,LIS+1);
+            if (res==-1){
+                dp[LIS++]=arr[idx];
+            }
+            else{
+                dp[res]=arr[idx];
+            }
+        }
+        System.out.println(LIS);
+    }
+    public static int binarySearch(int value, int start, int end, int size){
+        int res=0;
+        while (start<=end){
+            int mid=(start+end)/2;
+            if (value<=dp[mid]){
+                end=mid-1;
+                res=mid;
+            }
+            else{
+                start=mid+1;
+            }
+        }
+        if (start==size){
+            return -1;
+        }
+
+        return res;
     }
 }
